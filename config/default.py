9d3c82da33c5656b96a5716004dc83ad8c16ad10
Programming With Claudio <79118475+programmingwithclaudio@users.noreply.github.com>
2024-06-14 23:10:19 -0500

    Update default.py

--- config/default.py
+++ config/default.py
@@ -1,6 +1,6 @@
 SECRET_KEY = "so-secret"
 # postgresql://postgres:mailito@localhost:5432/dbservice
-# postgresql://postgres:mailito@192.168.100.11:5432/dbservice+
+
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 # Configuración adicional
